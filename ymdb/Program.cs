74d4fcc4ffed5746b2690cea29e50b9630752f02
Yahia Hamed <yahiamh309090@outlook.com>
2025-08-07 22:59:54 +0300

    .....

--- ymdb/Program.cs
+++ ymdb/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            // Clear the console to apply the background color to the entire window
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            string[] graph = {
+                "██    ██ ███    ███ ██████  ██████ ",
+                " ██  ██  ████  ████ ██   ██ ██   ██",
+                "  ████   ██ ████ ██ ██   ██ ██████ ",
+                "   ██    ██  ██  ██ ██   ██ ██   ██",
+                "   ██    ██      ██ ██████  ██████ "
+        };
+
+            foreach (string line in graph)
+            {
+                Console.WriteLine(line);
+            }
+            Console.ForegroundColor = ConsoleColor.Magenta;
         }
     }
 }
